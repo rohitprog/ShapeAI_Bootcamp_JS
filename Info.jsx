ae7cf5bf7c7c406f33d2ed425e1a314b06b6e61c
rohitprog <86481082+rohitprog@users.noreply.github.com>
2021-06-26 21:52:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A Basic Web Dev React JS Bootcamp.And this is the amazing Bootcamp ever I seen.And also I thanks to ShapeAI to conduct such as wonderful Bootcamp with free of cost...</p>
+    </div>
+  );
+}
+
+export default Info;
